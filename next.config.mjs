ed4177e7593e87703dd6e7c1674e1047208e29e8
Oscar Vale <oscar@prpl.se>
2024-02-11 20:00:07 +0000

    added sharp and set output to standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'shop-interview.acrowd.se',
-            pathname: '**/*',
-          },
-        ],
-      },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "shop-interview.acrowd.se",
+				pathname: "**/*",
+			},
+		],
+	},
+	output: "standalone",
 };
 
 export default nextConfig;
